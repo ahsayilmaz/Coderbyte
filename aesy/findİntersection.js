161967283a5017150418d43b5f2df8d6e7e52443
Ahmet Şahin Yılmaz <103384386+Emeralbladexy@users.noreply.github.com>
2023-09-28 20:22:09 +0300

    Add files via upload

---
+++ aesy/findİntersection.js
@@ -0,0 +1,7 @@
+export const intersection = (str) =>{
+    for (let i=0; i<str.length; i++){
+        str.replace(str[i], str[i].parseInt())
+    }
+    const intered= str.sort((a, b) =>{return b-a})
+    return intered
+}
