af51fffb6ae2961c4875e11cd38643ba2b9f8cb1
Ahmet Şahin Yılmaz <103384386+Emeralbladexy@users.noreply.github.com>
2023-09-28 20:24:54 +0300

    Update findİntersection.js

--- aesy/findİntersection.js
+++ aesy/findİntersection.js
@@ -2,6 +2,6 @@ export const intersection = (str) =>{
     for (let i=0; i<str.length; i++){
         str.replace(str[i], str[i].parseInt())
     }
-    const intered= str.sort((a, b) =>{return b-a})
+    const intered = str.sort((a, b) =>{return b-a})
     return intered
 }
