d3f22ec2679f97f6c9421a2af1e7adb879f6a407
Ahmet Şahin Yılmaz <103384386+Emeralbladexy@users.noreply.github.com>
2023-09-29 18:03:36 +0300

    Update findİntersection.js

--- aesy/findİntersection.js
+++ aesy/findİntersection.js
@@ -1,6 +1,6 @@
 export const intersection = (str) =>{
-    let listint = str.map(str => str.split(", "))
-    let flt = [].concat(...listint)
-    const intered = flt.sort((a, b) =>{return b-a})
-    return intered
+    let listint = str.map(str => str.split(", "));
+    let flt = [].concat(...listint);
+    const intered = flt.sort((a, b) =>{return b-a});
+    return intered;
 }
