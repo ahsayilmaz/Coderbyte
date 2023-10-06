39a70c1d85684d09a1058b7ade0da25dd6c4cde2
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-06 21:02:39 +0300

    Update easy.js

--- easy.js
+++ easy.js
@@ -2,4 +2,16 @@ import { longestWord } from "./aesy/longestWord.js";
 //console.log(longestWord("fun!!& time"))
 
 import { factorial } from "./aesy/factorial.js";
-console.log(factorial(8.256))
+//__console.log(factorial(8.256))
+import { reverse } from "./aesy/reversingString.js";
+//console.log(reverse("ahmet"))
+import { intersection } from "./aesy/findİntersection.js";
+//console.log(intersection(["1, 3, 4, 7, 13", "3, 1, 2, 4, 13, 15"]))
+import { attempt } from "./aesy/attempt.js";
+//console.log(attempt())
+import { question } from "./aesy/questionmark.js";
+//console.log(question("arrb6???4xxbl5???eee5"))
+import { ltrchanger } from "./aesy/letterChange.js";
+//console.log(ltrchanger("Ahmet Şahin"))
+import { adding } from "./aesy/simpleAdding.js";
+console.log(adding(-22))
