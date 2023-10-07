df675e810585e064fd517a1ea525e481583a9937
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-07 10:49:25 +0300

    Update letterCapitalize.js

--- aesy/letterCapitalize.js
+++ aesy/letterCapitalize.js
@@ -1,1 +1,6 @@
-
+export const Cap = (sentence) =>{
+    let list= sentence.split(" ")
+    let caped = list.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    let result = caped.join(' ')
+    return result
+}
