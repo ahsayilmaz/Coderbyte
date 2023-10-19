8696897f46b29bdb6281c8336f0ad669f09c53b6
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-19 19:24:09 +0300

    Update and rename attempt.js to 1-userValidation.js

--- aesy/attempt.js
+++
@@ -1,6 +0,0 @@
-export const attempt = () =>{
-    let fruits = ["banana", "apple", "orange", "mango"];
-    let split= fruits.map(fruit => fruit.split(/(,\s?)?a?/g));
-    let con = [].concat(...split)
-    console.log(con);
-}
