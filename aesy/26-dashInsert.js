47b539a2629ccc419c84d60700bbffe40d7f0199
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-19 21:01:58 +0300

    Update 26-dashInsert.js

--- aesy/26-dashInsert.js
+++ aesy/26-dashInsert.js
@@ -1,1 +1,9 @@
-
+export const dashInsert = (Input)=>{
+    let inputs = Input.split("")
+    for(let i = 0; i<inputs.length;i++){
+        if(/[0-9]/.test(inputs[i])&&(inputs[i]/2)%1!=0&&/[0-9]/.test(inputs[i+1])&&(inputs[i+1]/2)%1!=0){
+            inputs[i]=inputs[i]+"-"
+        }
+    }
+    return inputs.join("")
+}
