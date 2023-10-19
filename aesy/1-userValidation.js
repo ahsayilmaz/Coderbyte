a72e511cf0f2de2fb6dc427c94c0de26c1e61d06
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-19 19:31:47 +0300

    Update 1-userValidation.js

--- aesy/1-userValidation.js
+++ aesy/1-userValidation.js
@@ -1,8 +1,7 @@
-export const userValidation = (input) =>{
-  if(str.length>=4 && str.length<=25 && (/[a-zA-Z]/).test(str[0])&& (/\w([\-0-9])?/).test(str)
-&& (/_$/).test(userName)!==true){
-    console.log(true);
-}else{
-    console.log(false);
-}
-}
+export const userValidation = (str) =>{
+      if(str.length>=4 && str.length<=25 && (/[a-zA-Z]/).test(str[0])&& (/\w([\-0-9])?/).test(str) && (/_$/).test(str)!==true){
+        console.log(true);
+      }else{
+        console.log(false);
+      }
+  }
