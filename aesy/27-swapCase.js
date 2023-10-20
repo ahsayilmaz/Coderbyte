6f619a6d503a44009c34733920da8bf2bb07ed4e
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-20 09:18:03 +0300

    Update 27-swapCase.js

--- aesy/27-swapCase.js
+++ aesy/27-swapCase.js
@@ -1,1 +1,17 @@
-
+export const swapCase =(sentence) =>{
+    let arrayOfWords=sentence.split(" ")
+    for (let i = 0; i < arrayOfWords.length; i++) {
+        let arrayOfletters= arrayOfWords[i].split("")
+        for (let index = 0; index < arrayOfletters.length; index++) {
+            if(arrayOfletters[index].charCodeAt()>64&&arrayOfletters[index].charCodeAt()<91){
+                arrayOfletters[index]=arrayOfletters[index].toLowerCase()
+            }
+            else if(arrayOfletters[index].charCodeAt()>96&&arrayOfletters[index].charCodeAt()<123){
+                arrayOfletters[index]=arrayOfletters[index].toUpperCase()
+            }
+        }
+        let end = arrayOfletters.join('')
+        arrayOfWords[i]=end
+    }
+    return arrayOfWords.join(' ')
+}
