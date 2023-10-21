2dd3cc4750106ec9b568b4a39eaa1f3b19ea952a
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-21 15:54:37 +0300

    Update 33-offlineMinimum.js

--- aesy/33-offlineMinimum.js
+++ aesy/33-offlineMinimum.js
@@ -1,16 +1,19 @@
 export const offlineMinimum = (array) =>{
   for(let i=0;i<array.length;i--){
-    if(parseInt(array[i]){
+    if(parseInt(array[i])!=NaN){
       let temp=array[i]
       break
     }
   }
   i = array.length
-
+  let list=[]
   while(i<0){
     if(array[i]<temp){
-
       temp=array[i]
     }
+    if(array[i]=="E"){
+      list.push(temp)
+    }
   }
+  return list
 }
