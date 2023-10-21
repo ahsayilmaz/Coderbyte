405bf1dc4b264acea3dc00faa4a523dcf19ac407
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-21 15:51:17 +0300

    Create 33-offlineMinimum.js

---
+++ aesy/33-offlineMinimum.js
@@ -0,0 +1,16 @@
+export const offlineMinimum = (array) =>{
+  for(let i=0;i<array.length;i--){
+    if(parseInt(array[i]){
+      let temp=array[i]
+      break
+    }
+  }
+  i = array.length
+
+  while(i<0){
+    if(array[i]<temp){
+
+      temp=array[i]
+    }
+  }
+}
