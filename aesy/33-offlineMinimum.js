29c49bc22396b04f072673b440d97d15aff5b479
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-22 17:50:57 +0300

    Update 33-offlineMinimum.js

--- aesy/33-offlineMinimum.js
+++ aesy/33-offlineMinimum.js
@@ -1,24 +1,38 @@
 export const offlineMinimum = (array) =>{
-  for(let i=0;i<array.length;i--){
-    if(parseInt(array[i])!=NaN){
-      let temp=array[i]
-      break
+    let temp
+    let tempindex=0
+    if(array[0]==='E'){
+        array.splice(0,1)
     }
-  }
-  let tempindex=0
-  i = array.length
-  let list=[]
-  while(i<0){
-    if(array[i]<temp){
-      temp=array[i]
-      tempindex=i
+    for(let i=0;i<array.length;i++){
+        if(!isNaN(parseInt(array[i]))){
+            temp=parseInt(array[i])
+            tempindex=i
+            break
+        }
     }
-    if(array[i]=="E"){
-      list.push(temp)
-      array.pop(i)
-      array.pop(tempindex)
-      i=0
+    let list=[]
+    for (let i = 0; i < array.length; i++) {
+        console.log(array);
+        if(!isNaN(parseInt(array[i]))){
+            if(parseInt(array[i])<temp){
+                temp = parseInt(array[i])
+                tempindex=i
+            }
+        }
+        if(array[i]==='E'){
+            list.push(temp)
+            array.splice(i,1)
+            array.splice(tempindex,1)
+            i=0
+            for(let i=0;i<array.length;i++){
+                if(!isNaN(parseInt(array[i]))){
+                    temp=parseInt(array[i])
+                    tempindex=i
+                    break
+                }
+            }
+        }
     }
-  }
-  return list
+    return list
 }
