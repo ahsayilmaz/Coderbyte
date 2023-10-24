787fd464e4c3c34cc37b2be4ad88d56cbc1299e7
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-24 19:35:36 +0300

    Update 37-hammingDistance.js

--- aesy/37-hammingDistance.js
+++ aesy/37-hammingDistance.js
@@ -1,10 +1,11 @@
 export const hammingDistance =(input1, input2) =>{
-  input.toString().split()
-  let different=0
-  for(let i=0;i<input.length;i++){
-    if(input1[i]!=input[i]){
-      different++
+    let n1= input1.toString().split("")
+    let n2= input2.toString().split("")
+    let different=0
+    for(let i=0;i<n1.length;i++){
+        if(n1[i]!=n2[i]){
+            different++
+        }
     }
-  }
-  return different
+    return different
 }
