937e1ca9c3a2728b2cead581ddc430f6cfa71d8e
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-24 11:20:19 +0300

    Update 35-overlappingRanges.js

--- aesy/35-overlappingRanges.js
+++ aesy/35-overlappingRanges.js
@@ -1,1 +1,13 @@
-
+export const overLapping =(arr)=>{
+    for(let i =arr[0];i<=arr[1];i++){
+        for(let index =arr[2];index<=arr[3];index++){
+            if(index==i){
+                arr[4]--
+            }
+        }
+    }
+    if(arr[4]==0){
+        return true
+    }
+    return false
+}
