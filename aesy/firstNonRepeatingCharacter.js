59ddee52a8c0e883432ade1b821d30c2dd338ece
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-26 12:46:19 +0300

    Create firstNonRepeatingCharacter.js

---
+++ aesy/firstNonRepeatingCharacter.js
@@ -0,0 +1,16 @@
+export const firstNonRepeatingCharacter =(str)=>{
+    str=str.split("")
+    let letters =[{letter:str[0],repetition:1}]
+    for (let i = 1; i < str.length; i++) {
+        let letterToUpdate1 = letters.find(letters => letters.letter === str[i].toLowerCase());
+        let letterToUpdate = letters.find(letters => letters.letter === str[i]);
+        if (letterToUpdate||letterToUpdate1){
+            letterToUpdate.repetition+=1
+            console.log("a")
+        }else{
+            letters.push({letter:str[i],repetition:1})
+        }
+    }
+    let end=letters.find(letters => letters.repetition === 1);
+    return end.letter
+}
