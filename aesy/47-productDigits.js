86b1bf19c3479a06bb894bda7e186b9dbdffb9af
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-10-31 17:06:21 +0300

    Update 47-productDigits.js

--- aesy/47-productDigits.js
+++ aesy/47-productDigits.js
@@ -4,12 +4,8 @@ export const productDigits =(num)=>{
     for (let i = 1; i <=9; i++) {
         let digitslength=(num/i).toString().length+i.toString().length
         if(num%i==0&&digitslength==numl){
-            console.log(num/i)
-            console.log(i+"b")
             temp=digitslength   
         }else if(num%i==0&&digitslength<temp) {
-            console.log(num/i)
-            console.log(i+"a")
             temp=digitslength
         }
     }
@@ -18,8 +14,6 @@ export const productDigits =(num)=>{
         for (let i = 1; i+(9*((10**a)+1)) <= 9+(90*(10**a)); i++) {
             let digitslength=(num/i).toString().length+i.toString().length
             if(num%i==0&&digitslength<temp) {
-                console.log(num/i)
-                console.log(i)
                 temp=digitslength
             }
         }
