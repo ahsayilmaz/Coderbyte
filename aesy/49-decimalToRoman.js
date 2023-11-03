28af4c3b9dce318fc0d0e23cc23d6f3899165d1d
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-03 17:40:03 +0300

    Update 49-decimalToRoman.js

--- aesy/49-decimalToRoman.js
+++ aesy/49-decimalToRoman.js
@@ -1,21 +1,23 @@
-  export const decimalToRoman =(num)=>{
+export const decimalToRoman =(num)=>{
     let numl=num.toString().length
-    let decimals =[{number:num%10,digit:10**(numl-1)}]
+    let decimals =[]
     let Roman=[{digit:1,roman:"I"},{digit:5,roman:"V"},{digit:10,roman:"X"},
     {digit:50,roman:"L"},{digit:100,roman:"C"},{digit:500,roman:"D"},{digit:1000,roman:"M"}]
-    for (let i = numl-2; i>-1; i--) {
-        num=Math.floor(num/10)
+    for (let i = numl-1; i>-1; i--) {
         decimals.push({number:num%10,digit:10**(numl-i-1)})
+        num=Math.floor(num/10)
     }
     let string=""
     for (let i = decimals.length-1;i>-1;i--){
         let Rnum1 = Roman.find(Roman => Roman.digit === decimals[i].digit)
-        if(decimals[i].number<10){
-            if(decimals[i].number<4){
-                for (let index = 0; index < decimals[i].number; index++) {
-                    string=string+Rnum1.roman
-                }
+        if(decimals[i].number<4){
+            for (let index = 0; index < decimals[i].number; index++) {
+                string+=Rnum1.roman
             }
+        }
+        if(decimals[i].number==(5-1)||decimals[i].number==(10-1)){
+            let Rnum2 = Roman.find(Roman => Roman.digit === (decimals[i].digit*10))
+            string+=Rnum1.roman+Rnum2.roman
         }
     }
     return string
