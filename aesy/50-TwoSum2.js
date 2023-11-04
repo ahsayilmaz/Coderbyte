d15e6b4f935f0dd4473bb0238ee9f760d60bdc43
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-04 20:49:37 +0300

    Update 50-TwoSum2.js

--- aesy/50-TwoSum2.js
+++ aesy/50-TwoSum2.js
@@ -1,4 +1,5 @@
 export const TwoSum2 =(numArr, target)=>{
+    numArr.sort()
     for (let i = 0; i < numArr.length; i++) {
         let a = numArr.length-1
         while(numArr[i]+numArr[a]>target&&a>i&&numArr[i]!=numArr[a]) {
