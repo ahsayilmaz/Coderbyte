93164be478101cb332bd3d3c14e0aa2143401659
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-07 12:59:27 +0300

    Update 52-ScaleBalancing.js

--- aesy/52-ScaleBalancing.js
+++ aesy/52-ScaleBalancing.js
@@ -1,21 +1,29 @@
 export const ScaleBalancing=(arr1,arr2)=>{
     let scaler1=0
     arr2.sort((a,b)=>{return a-b})
+    arr1.sort((a,b)=>{return a-b})
     let scaler2=arr2.length-1
     let i=0
+    for (let i = 0; i < arr2.length; i++) {
+        arr1[1]-=arr2[i]
+        if(arr1[0]==arr1[1]){
+            return arr2[i]
+        }
+        arr1[1]+=arr2[i]
+    }
     arr1[0]+=arr2[scaler2]
     arr1[1]+=arr2[scaler1]
-    while(arr1[0]!=arr1[1]){
+    while(arr1[0]!=arr1[1]&&scaler1!=arr2.length-1){
+        if(scaler1>=scaler2||arr1[0]<arr1[1]){
+            scaler1++
+            scaler2=arr2.length-1
+        }
         arr1[0]-=arr2[scaler2]
         arr1[1]-=arr2[scaler1]
         scaler2--
-        scaler1++
-        if(scaler1>=scaler2||arr1[0]>arr1[1]){
-            scaler1++
-            scaler2=arr2.length-1
-        }else if(scaler1==arr2.length){return "nein"}
         arr1[0]+=arr2[scaler2]
         arr1[1]+=arr2[scaler1]
     }
-    return scaler1+","+scaler2
+    if(arr1[0]==arr1[1]){return arr2[scaler1]+","+arr2[scaler2]}
+    else{return "nein"}
 }
