21bc8f777a25041d46262ac929a364838621681e
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-10 22:08:31 +0300

    Create 55-vowelSquare.js

---
+++ aesy/55-vowelSquare.js
@@ -0,0 +1,10 @@
+export const vowelSquare =(list)=>{
+    for (let i = 0; i < list.length-1; i++) {
+        for (let index = 0; index < list[i].length; index++) {
+            if((/[a||i||o||u||e]/.test(list[i][index]) && /[a||i||o||u||e]/.test(list[i+1][index]))&&(/[a||i||o||u||e]/.test(list[i][index]) && /[a||i||o||u||e]/.test(list[i+1][index+1]))){
+                return list[i][index]+list[i][index+1]+"\n"+list[i+1][index]+list[i+1][index+1]
+            }
+        }
+    }
+    
+}
