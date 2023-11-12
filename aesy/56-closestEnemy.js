66677aecfb695c10fb49295729e206b818956d7b
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-12 18:35:59 +0300

    Update 56-closestEnemy.js

--- aesy/56-closestEnemy.js
+++ aesy/56-closestEnemy.js
@@ -1,17 +1,28 @@
 export const closestEnemy =(arr)=>{
     let enemy=0
     let comrad
+    let uzaklik=0
     for(let i =0;i<arr.length;i++){
         if(arr[i]===1){
             comrad=i
         }
-        else if(arr[i]===2&&enemy==0){
-            enemy=i
+    }
+    
+    for(let i =0;i<arr.length;i++){
+        if(arr[i]===2&&enemy==0){
+            let geciciuzaklik = Math.abs(i-comrad)
+            if (uzaklik == 0){
+                uzaklik = geciciuzaklik
+                enemy = i
+            } 
+            if(geciciuzaklik!=0 && uzaklik !=0 && (geciciuzaklik < uzaklik)){
+                enemy = i
+                uzaklik = geciciuzaklik
+            }
         }
     }
-    if(enemy-comrad<0){
-        return (enemy-comrad)*-1
-    }else{
-        return enemy-comrad
-    }
+
+
+
+    return uzaklik
 }
