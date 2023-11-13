861d7bb9a02ab19ecad94d854492d62e15d4840b
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-13 14:30:58 +0300

    Update 54-pascalTriangle.js

--- aesy/54-pascalTriangle.js
+++ aesy/54-pascalTriangle.js
@@ -1,15 +1,21 @@
 export const pascalTriangle =(Num)=>{
     let pascal=[[0,1,0]]
-    for (let i = 0; i < Num-1; i++) {
+    for (let i = 0; i < Num; i++) {
         pascal.push([0])
         for (let index = 0; index < pascal[i].length-1; index++) {
             pascal[pascal.length-1].push(pascal[i][index]+pascal[i][index+1])
         }
         pascal[pascal.length-1].push(0)
     }
-    for (let i = 0; i < Num-1; i++) {
-        for (let index = 1; index < pascal[i].length-2; index++) {
-            console.log(pascal[i][index])
+    for (let i = 0; i < Num; i++) {
+        let str=""
+        for (let a = 0; a < Num-i; a++) {
+            str+=" "
         }
+        for (let index = 1; index < pascal[i].length-1; index++) {
+            str+=pascal[i][index]
+            str+=" "
+        }
+        console.log(str);
     }
 }
