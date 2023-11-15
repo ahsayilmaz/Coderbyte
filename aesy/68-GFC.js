8ccd78840346892af88ed116ff0369965b22d91c
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-15 22:28:28 +0300

    Update 68-GFC.js

--- aesy/68-GFC.js
+++ aesy/68-GFC.js
@@ -1,11 +1,14 @@
 export const GFC=(arr)=>{
     arr.sort((a,b)=>{return a-b})
-    let temp=1
+    let divider=0
+    let dividerIndex=1
     for (let i = 1; i < arr[0]; i++) {
-        let divider=arr[0]/i
-        if(arr[0]%divider==0&&divider>temp&&arr[1]%divider==0){
-            temp=divider
+        let tempDivider=arr[0]/i
+        if(arr[0]%tempDivider==0&&tempDivider>divider&&arr[1]%tempDivider==0){
+            divider=tempDivider
+            dividerIndex=i
         }
+        if(dividerIndex>=tempDivider){break}
     }
-    return temp
+    return divider
 }
