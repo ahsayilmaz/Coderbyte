676234f34866d8c36749dbbd654442ccc5511889
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-19 20:13:58 +0300

    Create 72-removeBrackets.js

---
+++ aesy/72-removeBrackets.js
@@ -0,0 +1,22 @@
+export const removeBrackets = (str) => {
+    str = str.replace(/[^\{\[\(\)\]\}]/g, "").split("");
+    const stack = [];
+
+    for (let char of str) {
+        if (char === "[" || char === "(" || char === "{") {
+            stack.push(char);
+        } else {
+            const lastBracket = stack.pop();
+
+            if (
+                (char === "]" && lastBracket !== "[") ||
+                (char === ")" && lastBracket !== "(") ||
+                (char === "}" && lastBracket !== "{")
+            ) {
+                return 0; // brackets are not balanced
+            }
+        }
+    }
+
+    return stack.length === 0 ? 1 : 0;
+};
