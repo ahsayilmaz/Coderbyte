ddbc2b95d50437d9b83a1c67c2a3534e6507d31e
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-25 10:52:55 +0300

    Update medium.js

--- medium.js
+++ medium.js
@@ -1,7 +1,2 @@
 import { primeTime } from "./medium/primeTime.js";
-//console.log(primeTime(65521));
-for (let i = 2; i <= Math.pow(2, 16); i++) {
-    if (primeTime(i)) {
-        console.log(i);
-    }
-}
+//console.log(primeTime(65536));
