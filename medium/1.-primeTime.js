2ecea8dfe4cfc5f7c63b0d56ac104b5d5048fcdf
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-26 15:28:51 +0300

    Update 1.-primeTime.js

--- medium/1.-primeTime.js
+++ medium/1.-primeTime.js
@@ -1,21 +1,15 @@
 export const primeTime = (n) => {
+    n++
     let primes = new Array(n).fill(true);
     primes[0] = primes[1] = false;
 
     for (let i = 2; i <= Math.sqrt(n); i++) {
         if (primes[i]) {
-            for (let j = i * i; j < n; j += i) {
+            for (let j = i * i; j <n; j += i) {
                 primes[j] = false;
             }
         }
     }
 
-    let count = 0;
-    for (let i = 2; i < n; i++) {
-        if (primes[i]) {
-            count++;
-        }
-    }
-
-    return count;
+    return primes[n-1]
 }
