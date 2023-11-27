4854f7ad270dd7c181b7b86e053c631ac3991ee2
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-27 22:32:59 +0300

    Update medium.js

--- medium.js
+++ medium.js
@@ -1,4 +1,6 @@
 import { primeTime } from "./medium/primeTime.js";
 //console.log(primeTime(65536));
 import { runLength } from "./medium/runLength.js";
-console.log(runLength("wwwrrhg"));
+//console.log(runLength("wwwrrhg"));
+import { primeMover } from "./medium/primeMover.js";
+console.log(primeMover(23));
