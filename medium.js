c80099c500fac575f8c76302d010329c863f11e8
Ahmet Şahin Yılmaz <103384386+ahsayilmaz@users.noreply.github.com>
2023-11-28 20:01:20 +0300

    Update medium.js

--- medium.js
+++ medium.js
@@ -3,4 +3,6 @@ import { primeTime } from "./medium/primeTime.js";
 import { runLength } from "./medium/runLength.js";
 //console.log(runLength("wwwrrhg"));
 import { primeMover } from "./medium/primeMover.js";
-console.log(primeMover(23));
+//console.log(primeMover(23));
+import { stringScramble } from "./medium/stringScramble.js";
+//console.log(stringScramble(["rkqodlw","world"]));
